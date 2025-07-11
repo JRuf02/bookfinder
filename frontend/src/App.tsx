fa3c6ccf851b9f934da8739bce50212f43e52c59
JRuf02 <julian.g.ruf@web.de>
2025-07-11 13:09:26 +0000

    removed unused imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,16 @@
-import { useState, useRef } from "react";
-import { fetchBookData } from "./services/fetchBookData";
 import "./styles/global.css";
 
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeScreen from "./pages/HomeScreen";
 import ScanningScreen from "./pages/ScanningScreen";
-/*import ScannerResultScreen from "./pages/ScannerResultScreen";
-import ShelfActionScreen from "./pages/ShelfActionScreen";
-import ManualAddScreen from "./pages/ManualAddScreen";
+/*import ManualAddScreen from "./pages/ManualAddScreen";
 import InfoScreen from "./pages/InfoScreen";
 import CatalogHomeScreen from "./pages/CatalogHomeScreen";*/
 import NotFoundScreen from "./pages/NotFoundScreen";
 import BottomNavBar from "./components/BottomNavBar";
 
-type ShelfAction = "insert" | "remove" | null;
-
 function App() {
   /*
-        <Route path="/result" element={<ScannerResultScreen />} />
-        <Route path="/shelf-action" element={<ShelfActionScreen />} />
         <Route path="/manual-add" element={<ManualAddScreen />} />
         <Route path="/info" element={<InfoScreen />} />
         <Route path="/catalog" element={<CatalogHomeScreen />} />
